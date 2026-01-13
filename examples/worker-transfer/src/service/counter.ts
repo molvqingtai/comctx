eb1fb6ed1e1850bfedcce1a747e5428208a632e6
molvqingtai <molvqingtai@gmail.com>
2026-01-13 21:26:27 +0800

    docs: better streaming example

--- examples/worker-transfer/src/service/counter.ts
+++ examples/worker-transfer/src/service/counter.ts
@@ -1,42 +1,31 @@
 import { defineProxy } from 'comctx'
 
 class Counter {
-  public value = new ArrayBuffer(4)
+  public value: number
+  private stream: ReadableStream<number>
+  private writer: WritableStreamDefaultWriter<number>
 
   constructor(initialValue: number = 0) {
-    const view = new Int32Array(this.value)
-    view[0] = initialValue
+    this.value = initialValue
+    const { readable, writable } = new TransformStream<number, number>()
+    this.stream = readable
+    this.writer = writable.getWriter()
   }
 
   async getValue() {
-    // Return a copy of the buffer
-    const copy = new ArrayBuffer(4)
-    new Int32Array(copy)[0] = new Int32Array(this.value)[0]
-    return copy // Zero-copy transferred
-
-    // return this.value
+    return this.value
   }
 
-  async onChange(callback: (value: ArrayBuffer) => void) {
-    let oldValue = new Int32Array(this.value)[0]
-
-    setInterval(async () => {
-      const newValue = new Int32Array(this.value)[0]
-      if (oldValue !== newValue) {
-        callback(await this.getValue())
-        oldValue = newValue
-      }
-    }, 100)
+  async getStream() {
+    return this.stream
   }
 
   async increment() {
-    new Int32Array(this.value)[0]++
-    return this.getValue()
+    return await this.writer.write(++this.value)
   }
 
   async decrement() {
-    new Int32Array(this.value)[0]--
-    return this.getValue()
+    return await this.writer.write(--this.value)
   }
 }
 
